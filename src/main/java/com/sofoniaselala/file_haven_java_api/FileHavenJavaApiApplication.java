2da8727d36a823fd4d140101bd04e73f95eff256
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-12-05 20:13:55 -0500

    setup spring-boot project with postgresql

---
+++ src/main/java/com/sofoniaselala/file_haven_java_api/FileHavenJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.sofoniaselala.file_haven_java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileHavenJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileHavenJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofoniaselala/file_haven_java_api/FileHavenJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofoniaselala.file_haven_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHavenJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
