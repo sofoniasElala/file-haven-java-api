bacb3035a835993ab755d65d87ad47f7476d0955
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-12-31 16:21:51 -0500

    add a signup route

--- src/main/java/com/sofoniaselala/file_haven_java_api/Controller/UserController.java
+++ src/main/java/com/sofoniaselala/file_haven_java_api/Controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +19,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.sofoniaselala.file_haven_java_api.Controller.DTOs.LoginRequest;
+import com.sofoniaselala.file_haven_java_api.Controller.DTOs.SignupRequest;
 import com.sofoniaselala.file_haven_java_api.Helpers.AppUserDetails;
 import com.sofoniaselala.file_haven_java_api.Model.User;
 import com.sofoniaselala.file_haven_java_api.Repository.UserRepository;
 import com.sofoniaselala.file_haven_java_api.Services.JwtService;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -31,11 +35,13 @@ public class UserController {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final  PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository, AuthenticationManager authenticationManager, JwtService jwtService){
+    public UserController(UserRepository userRepository, AuthenticationManager authenticationManager, JwtService jwtService, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
 
     }
 
@@ -58,9 +64,9 @@ public class UserController {
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public Map<String, Object> login(@RequestBody User user, HttpServletResponse response){
+    public Map<String, Object> login(@Valid @RequestBody LoginRequest request, HttpServletResponse response){
         Map<String, Object> responseBody = new HashMap<>();
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.username(), request.password()));
     if(authentication.isAuthenticated()){
         String accessToken = jwtService.GenerateToken(((AppUserDetails)authentication.getPrincipal()).getId());
         // set accessToken to cookie header
@@ -77,6 +83,32 @@ public class UserController {
     } else {
         responseBody.put("success", false);
     }
+    return responseBody;
+
+    }
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Object> signup(@Valid @RequestBody SignupRequest request, HttpServletResponse response){
+        Map<String, Object> responseBody = new HashMap<>();
+        if(!request.password().equals(request.confirmPassword())) {
+             responseBody.put("success", false);
+             responseBody.put("message", "Passwords don't match");
+        } else {
+             String username = request.username();
+             Optional<User> existingUser = this.userRepository.findByUsernameIgnoreCase(username);
+            if (existingUser.isPresent()) {
+                responseBody.put("success", false);
+                responseBody.put("message", String.format("User with the email address '%s' already exists.", username));
+            } else {
+                String hashedPassword = passwordEncoder.encode(request.password());
+                User user = new User(username, hashedPassword);
+                this.userRepository.save(user);
+                responseBody.put("success", true);
+            }
+        }
+
+
     return responseBody;
 
     }
