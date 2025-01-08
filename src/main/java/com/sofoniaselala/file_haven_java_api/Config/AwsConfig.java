a6287d07473ee8035ca1e596fa576359d7859a0a
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2025-01-07 19:55:54 -0500

    load aws cred from environment variables

--- src/main/java/com/sofoniaselala/file_haven_java_api/Config/AwsConfig.java
+++ src/main/java/com/sofoniaselala/file_haven_java_api/Config/AwsConfig.java
@@ -3,7 +3,7 @@ package com.sofoniaselala.file_haven_java_api.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -12,10 +12,9 @@ public class AwsConfig {
 
     @Bean
     public S3Client s3Client() {
-        // Automatically picks up credentials from ~/.aws/credentials
         return S3Client.builder()
                 .region(Region.US_EAST_2) 
-                .credentialsProvider(ProfileCredentialsProvider.create()) // Use 'default'
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create()) // Use environment variables AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY
                 .build();
     }
 }
